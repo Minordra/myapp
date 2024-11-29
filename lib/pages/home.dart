28e323a1e3aea22090ccecdcdcdbabe77e57614a
pantakran.n63 <pantakran.n63@gmail.com>
2024-11-29 04:30:24 +0000

    first commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(    
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text('Breakfast',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),  
+      ),
+      centerTitle: true,
+    ),
+    );
+  }
+}
